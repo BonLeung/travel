f78d28b057774c354b7f557709b5acce17a3445b
BonLeung <654592397@qq.com>
2020-06-11 00:04:57 +0800

    finish city-search

---
+++ src/views/city/components/Search.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名称">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/styles/variable.scss';
+
+.search {
+  height: .72rem;
+  padding: 0 .1rem;
+  background-color: $bgColor;
+  display: flex;
+  align-items: center;
+  .search-input {
+    width: 100%;
+    height: .6rem;
+    box-sizing: border-box;
+    line-height: .62rem;
+    text-align: center;
+    border: none;
+    border-radius: .06rem;
+    outline: none;
+    color: #666;
+  }
+}
+</style>
